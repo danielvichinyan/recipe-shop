d8db716ad258ec223732ff6a21104e15411c559a
dvichinyan <Edlanta301>
2021-09-01 17:37:54 +0300

    Implemented auth service - sign up method works. Implemented loading spinner and added it to the auth page. Started login method but still not implemented

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,25 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
   isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true; // start spinner 
+
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (response) => {
+          this.isLoading = false; // stop spinner 
+        },
+        errorMessage => {
+          this.error = errorMessage;
+          this.isLoading = false; // stop spinner 
+        }
+      );
+    }
+
     form.reset();
   }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJU_ALm-Pk-3GohsyoBktprCwWiEsi2V8`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error ocurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email already exists!';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJU_ALm-Pk-3GohsyoBktprCwWiEsi2V8`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    );
+  }
+}
